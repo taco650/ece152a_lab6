ca43e841399c4c66a07e4dd2cf7e094302b13b88
taco650 <taco650@gmail.com>
2020-05-21 16:08:40 -0700

    Finished input controller

--- design.sv
+++ design.sv
@@ -22,6 +22,27 @@ module controller ( input clk,
     // 3. Monitor start_button ( analogous to enable in Lab4 )
     // 4. state_display consists of 4 - alphabets based on the state of the controller . Use 7 - segment display representation to display the state .
     
+    //state machine does not need power as input?
+    statemachine statemachine(.clk(clk),
+                              .power(valid_power),
+                              .timer(timerCount),
+                              .door_status(door_status),
+                              .start_button(start_button),
+                              .cancel_button(cancel_button),
+                              .state(state));
+    
+    inputController inputController(.state(state),
+                                    .power(power),
+                                    .timerInput(timer),
+                                    .validPower(valid_power),
+                                    .validTimer(valid_timer));
+                                
+    timercounter timercounter(  .clk(clk), 
+                                .timer(timer), 
+                                .writeEnable(), 
+                                .decrEnable(), 
+                                .reset(),
+                                .timercount(timerCount));
 
 
 endmodule
@@ -31,10 +52,9 @@ module statemachine(input clk,
                     input [6:0] timer,
                     input door_status,
                     input start_button,
-                    input cancel_button);
+                    input cancel_button, 
+                    output reg[4:0] state);
     
-    reg[4:0] state; //alllows for 2^5 different states
-
     always @(posedge clk or cancel_button or door_status) begin
         if(cancel_button == 1) begin
             state = 0; //reset timer and return to S0
@@ -48,11 +68,15 @@ module statemachine(input clk,
                 state = 0;
             end
             
+            else if(start_button == 1 && state == 1 && power == 1) begin
+                state = 6; //microwaving/processing state on HIGH POWER    
+            end
+
             else if(start_button == 1 && state == 1) begin
-                state = 2; //microwaving/processing state    
+                state = 2; //microwaving/processing state on HALF POWER
             end
             
-            else if(door_status == 0 && (state == 2 || state == 4) )begin
+            else if(door_status == 0 && (state == 2 || state == 4 || state == 6) )begin
                 state = 3; //stopped processing state (no programming allowed)
             end
 
@@ -60,11 +84,15 @@ module statemachine(input clk,
                 state = 4; //reprogram processing state (allowed reprogramming)
             end
 
+            else if(start_button == 1 && state == 4 && power == 1)begin
+                state = 6;//microwaving/processing state on HIGH POWER
+            end
+
             else if(start_button == 1 && state == 4)begin
-                state = 2;//microwaving/processing state
+                state = 2;//microwaving/processing state on LOW POWER 
             end
             
-            else if(timer == 0 && state == 2)begin //timer counter == 0 
+            else if(timer == 0 && (state == 2 || state == 6))begin //timer counter == 0 
                 state = 5;//Done state
             end
             
@@ -75,6 +103,40 @@ module statemachine(input clk,
         end
     end
 
+
+endmodule
+
+module inputController( input [4:0] state,
+                        input power,
+                        output reg validPower,
+                        output decrEnable,
+                        output writeEnable,
+                        output reset);
+    always @(*)begin
+        if(state == 1 || state == 4) begin
+            validPower = power;
+            decrEnable = 0;
+            writeEnable = 1;
+            reset = 0;
+        end
+        else if(state == 0)begin
+            //default values
+            validPower = 0;
+            decrEnable = 0;
+            writeEnable = 0;
+            reset = 1;
+        end
+        else if(state == 6 || state == 2)begin
+            decrEnable = 1;
+            writeEnable = 0;
+            reset = 0;
+        end
+        else if(state == 3)begin
+            decrEnable = 0;
+            writeEnable = 0;
+            reset = 0;
+        end
+    end
 
 endmodule
 
